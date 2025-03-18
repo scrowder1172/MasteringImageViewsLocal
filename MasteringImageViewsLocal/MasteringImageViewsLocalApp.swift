a20c6d4e8d325fe2891c27cf9f667148958c7954
Scott Crowder <scrowder@gmail.com>
2025-03-18 13:39:25 -0500

    Initial Commit

---
+++ MasteringImageViewsLocal/MasteringImageViewsLocalApp.swift
@@ -0,0 +1,20 @@
+//
+// File: MasteringImageViewsLocalApp.swift
+// Project: MasteringImageViewsLocal
+// 
+// Created by SCOTT CROWDER on 3/18/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct MasteringImageViewsLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
